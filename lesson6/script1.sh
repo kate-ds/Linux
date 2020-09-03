b4797fbb77803185fac043a76ef8a74cd0139f10
Ekaterina <esokolova@mindthedigital.com>
2020-09-03 16:00:49 +0300

    Update

--- lesson6/script1.sh
+++ lesson6/script1.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
-OUTPUT=$(sed -e '/^$/d; s/\(.*\)/\U\1/'  $1)
-echo $OUTPUT
-if $2
 
+OPTIONS=w
+LONGOPTS=write,help
+
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+
+eval set -- "$PARSED"
+
+write=0 help=0
+
+while true; do
+    case "$1" in
+ -w|--write)
+     write=1
+     shift
+     ;;
+ --help)
+     help=1
+     shift
+     ;;
+ --)
+     shift
+     break
+ ;;
+ *)
+     echo "Programming error"
+     exit 3
+     ;;
+ esac
+done
+
+# echo "help: $help, write: $write, "
+
+if [ $help -eq 1 ]
+then
+    echo "Arguments -w writes to file"
+    exit
+fi
+
+if [ $write -eq 0 ]
+then
+	OUTPUT=$(sed -e '/^$/d; s/\(.*\)/\U\1/' $1)
+	echo $OUTPUT
+else
+	sed -i '/^$/d; s/\(.*\)/\U\1/' $1
+fi
